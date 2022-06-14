731b10e660f16d308db3bd21926186c85f2058f3
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-06-14 20:58:41 +0100

    add postsync pipeline triggers

---
+++ components/argocd-hooks/files/postsync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+TEMPLATE_FILE="$SCRIPT_DIR/webhook-template.json"
+
+curl -sX POST \
+  -H "Content-Type: application/json" \
+  -d "$(envsubst < "$TEMPLATE_FILE")" \
+  "$WEBHOOK_URL"
